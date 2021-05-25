4ef3657250467b7c6d90f0039242aab34afa8780
Jacob Windsor <me@jcbwndsr.com>
2021-05-25 11:32:40 +0200

    Move projects service to projects-list component

--- projects-index/src/app/projects-list/projects.service.ts
+++ projects-index/src/app/projects-list/projects.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {map} from "rxjs/operators";
 import {Observable} from "rxjs";
-import {environment} from "../environments/environment";
+import {environment} from "../../environments/environment";
 
 //todo: fix up this service
 
