ed25db7d721f5c9ec0f86079677fdb52bc5a0b5a
Jacob Windsor <me@jcbwndsr.com>
2021-05-25 13:22:26 +0200

    Fix typo

--- projects-index/src/app/app-routing.module.ts
+++ projects-index/src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes.concat())],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
