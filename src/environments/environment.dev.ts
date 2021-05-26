de67bec1a1cbe2f927ff959d786a450e0a75896e
Jacob Windsor <me@jcbwndsr.com>
2021-05-26 12:49:26 +0200

    Add staging and dev environments

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  ingestApiUrl: 'https://api.ingest.dev.archive.data.humancellatlas.org/',
+  catalogueEndpoint: '/projects/search/catalogue?page=0&size=200',
+  wranglerEmail: 'wrangler-team@data.humancellatlas.org',
+};
