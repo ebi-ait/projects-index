14601cf064bc572d55355c771b854fa8440d62c2
Jacob Windsor <me@jcbwndsr.com>
2021-06-03 12:18:09 +0200

    Log global error

--- src/app/errors/global-error.service.ts
+++ src/app/errors/global-error.service.ts
@@ -6,6 +6,7 @@ export class GlobalErrorService implements ErrorHandler {
   constructor(private router: Router) {}
 
   handleError(error: Error) {
+    console.log(error);
     this.router.navigate(['/error']);
   }
 }
