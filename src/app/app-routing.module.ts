8af7f07d207c2a30bd44e18d1a6059156d8db35a
Jacob Windsor <me@jcbwndsr.com>
2021-06-03 12:17:34 +0200

    Redirect to 404 page if route not matched

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ErrorNotFoundComponent } from './errors/error-not-found/error-not-found.component';
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
@@ -13,7 +14,8 @@ const routes: Routes = [
     path: '',
     loadChildren: () =>
       import('./projects/projects.module').then((m) => m.ProjectsModule),
-  }
+  },
+  { path: '**', component: ErrorNotFoundComponent }
 ];
 
 @NgModule({
