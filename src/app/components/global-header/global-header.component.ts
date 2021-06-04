0a041670d52fa4bb8b1e2232a75689b88b74af8e
Karoly Erdos <karoly@ebi.ac.uk>
2021-06-04 15:46:26 +0100

    Modify title and text on header based on the route

--- src/app/components/global-header/global-header.component.ts
+++ src/app/components/global-header/global-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-global-header',
@@ -6,7 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./global-header.component.css'],
 })
 export class GlobalHeaderComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if(event instanceof NavigationEnd && event.url) {
+        this.activatedRoute = event.url;
+      }
+    });
+  }
 
   ngOnInit(): void {}
+
+  activatedRoute = '';
+  showAboutLink = true;
+
+  get headerTitle() {
+      if (this.activatedRoute.includes('about')) {
+        return 'About the Catalogue';
+      } else {
+        return 'HCA Project Catalogue';
+      }
+  }
+
+  get headerText() {
+    if (this.activatedRoute.includes('about')) {
+      this.showAboutLink = false;
+      return 'Aims, eligibility criteria and selection process';
+    } else {
+      this.showAboutLink = true;
+      return 'A comprehensive list of cellular resolution datasets for the Human Cell Atlas.';
+    }
+  }
 }
