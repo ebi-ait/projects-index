0cc7055ad25c1deae3e7c8094f9a7fa70eb1d56f
Jacob Windsor <me@jcbwndsr.com>
2021-06-08 14:31:52 +0200

    Revert "<hotfix> Set staging page size to 1000"
    This reverts commit 95d09c35ef88a061f25ef0b2619f71ba316f9703.

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   ingestApiUrl: 'https://api.ingest.staging.archive.data.humancellatlas.org/',
-  catalogueEndpoint: '/projects/search/catalogue?page=0&size=500',
+  catalogueEndpoint: '/projects/search/catalogue?page=0&size=200',
   wranglerEmail: 'wrangler-team@data.humancellatlas.org',
 };
