1d7441514f09900e944c3c538c11235c770bdae2
Jacob Windsor <me@jcbwndsr.com>
2021-06-11 11:49:43 +0200

    Remove extra slash

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  ingestApiUrl: 'https://api.ingest.dev.archive.data.humancellatlas.org/',
+  ingestApiUrl: 'https://api.ingest.dev.archive.data.humancellatlas.org',
   catalogueEndpoint: '/projects/search/catalogue?page=0&size=200',
   wranglerEmail: 'wrangler-team@data.humancellatlas.org',
 };
