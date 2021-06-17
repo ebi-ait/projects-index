6517670b862a93398925af7eb3f311874f64c78e
Jacob Windsor <me@jcbwndsr.com>
2021-06-17 16:07:01 +0200

    Revert "Remove breadcrumbs"
    This reverts commit bad7993b0632e8873e994f0f48fed203db8b1b6c.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+
+  isAboutPage(): boolean {
+    var url = window.location.href;
+    var lastPart = url.substr(url.lastIndexOf('/') + 1);
+
+    return lastPart === "about" ?? false
+  }
+}
