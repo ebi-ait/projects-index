bad7993b0632e8873e994f0f48fed203db8b1b6c
Jacob Windsor <me@jcbwndsr.com>
2021-06-17 16:07:01 +0200

    Remove breadcrumbs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-
-  isAboutPage(): boolean {
-    var url = window.location.href;
-    var lastPart = url.substr(url.lastIndexOf('/') + 1);
-
-    return lastPart === "about" ?? false
-  }
-}
+export class AppComponent {}
