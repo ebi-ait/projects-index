d547de5017a666e13f8790b03f535c6fa4f3455c
Amnon Khen <amnon@ebi.ac.uk>
2021-06-23 14:09:48 +0100

    mini-cards for showing a single number

--- src/app/summary/summary.module.ts
+++ src/app/summary/summary.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import {SummaryComponent} from "./summary.component";
 import {ChartComponent} from "./chart/chart.component";
 import {ChartsModule} from "ng2-charts";
+import {MiniCardComponent} from "./mini-card/mini-card.component";
 
 
 
 @NgModule({
-  declarations: [SummaryComponent, ChartComponent],
+  declarations: [SummaryComponent, ChartComponent, MiniCardComponent],
   imports: [
     CommonModule,
-    ChartsModule
+    ChartsModule,
   ],
-  exports: [SummaryComponent, ChartComponent]
+  exports: [SummaryComponent, ChartComponent, MiniCardComponent]
 })
 export class SummaryModule { }
