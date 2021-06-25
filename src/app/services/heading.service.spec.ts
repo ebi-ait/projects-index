5c2424c3ff8953aefc6e4bb630e231309e3fa8e3
Jacob Windsor <me@jcbwndsr.com>
2021-06-25 16:20:05 +0200

    Fix typo

--- src/app/services/heading.service.spec.ts
+++ src/app/services/heading.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { HeadingService } from './heading.service';
 
-describe('HeadingServiceService', () => {
+describe('HeadingService', () => {
   let service: HeadingService;
 
   beforeEach(() => {
