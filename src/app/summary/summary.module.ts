0f90e530e31b7526fffbb83faded542c04d38189
Amnon Khen <amnon@ebi.ac.uk>
2021-06-28 17:11:55 +0100

    code review fixes: spaces, quotes, summary breadcrumbs, signature,

--- src/app/summary/summary.module.ts
+++ src/app/summary/summary.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ChartComponent} from './components/chart/chart.component';
-import {ChartsModule} from "ng2-charts/public_api";
 import {MiniCardComponent} from './components/mini-card/mini-card.component';
 import {SummaryComponent} from './summary.component';
 import {SummaryService} from './summary.service';
 import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {fas} from '@fortawesome/free-solid-svg-icons';
+import {ChartsModule} from "ng2-charts";
 
 @NgModule({
             declarations: [
